2a3d7639a191bd75c08c630dcc13a96f48763e2d
Student <Student@Train11-2-PC359.control.ad.sit.kmutt.ac.th>
2025-07-22 16:33:35 +0700

    Sattiphai

---
+++ Class2_worlshop.py/konsong.py
@@ -0,0 +1,14 @@
+Raja_1 = (float(input("ระยะทางเท่าใด")))
+
+if Raja_1 >= 501:
+    print("45ฮาฟฟฟฟ")
+elif Raja_1 >= 301:
+    print("35บาทฮาฟ")
+elif Raja_1 >= 101:
+    print("25บาทฮาฟ")
+elif Raja_1 >= 51:
+    print("15บาทฮาฟ")
+elif Raja_1 >= 5:
+    print("10บาทฮาฟฟุ่ววววว")
+else:
+    print("ไม่ไปโว้ย")
---
+++ class2.py
@@ -0,0 +1,150 @@
+x = 1
+
+print(x)
+
+x = 4 
+
+print(x)
+
+age = 19
+money = 300
+zeroo = 0
+
+print(type(age))
+print(type(money))
+print(type(zeroo))
+
+#float
+height = 176.99
+profit = 200.99
+zeroox = 3.1
+
+print(type(zeroox))
+print(type(profit))
+print(type(height))
+
+cat = True
+dog = False
+
+isCat = True
+
+print(type(cat))
+print(type(cat))
+print(type(cat))
+
+#string
+Hello = "Brother"
+Phone = "Kanchai"
+tell = "i'am batman"
+
+print(type(Hello))
+print(type(Phone))
+print(type(tell))
+
+#type conversion แปลงชนิดของคำ
+#str --> int
+abc = "100" #str
+cba = 100 # int
+int(abc)
+#float(abc)
+#str(abc)
+#bool(abc)
+print(int(abc) + 50)
+#str --> int
+
+jju = 10.9 #float
+print(int(jju) + 50 )
+
+jud = 10.89
+print("this is judy: ", jud)
+
+a = "120"
+b = "180"
+
+print("a + b :",int(a) + int(b))
+
+x2 = "33.33"
+print(type(x2))
+
+float()
+
+teddy = 15
+print(float(teddy))
+
+#bear = "big Bear"
+#print(int(bear))
+
+#str
+
+num = 191
+print(type(str(num)))
+
+#operation
+print(12+8)
+print("the One Piece" + "its Real!!!!!")
+
+print("lakya" * 5)
+
+
+#=====[%]=====
+#หารเอาเศษ
+print(4%2)
+print(10%3)
+#เอาไว้หาเลขคุ๋-คี่
+#print(===%2)
+print("Total:",8%2)
+
+#ยกกำลัง= **
+
+#compare operator
+# == , != ,> ,<,=<,=>
+#เท่ากัน,ไม่เท่ากัน,มากกว่า,น้อยกว่า,น้อยกว่าหรือเท่ากับ,มากกว่าหรือเท่ากับ
+
+o1 = 5
+o2 = 10
+# == เท่ากัน
+print("===[]====")
+print(o1 == 5)
+print(o1 == 4)
+
+#  != ไม่เท่ากับ
+print("====[!=]====")
+print(o1 != 10)
+
+
+#<น้อยกว่า
+print("===[<]===")
+print(o1 < o2)
+print(o2 < -20)
+
+#<= =>
+print("===[>=]===")
+print(o2 >= 10)
+
+#boolean operators สัญลักษณ์ทางตรรกศาสตร์
+
+
+P = True
+q = False
+print("===[boolean]===")
+print (P and q)
+print (q and q)
+print("===[or]===")
+print(P or q) #มีตัวไหนเป็นtrue คือtrueทุกตัว 
+print(False or False) #false
+
+whatUP = input("my name is :")
+print(whatUP)
+
+#condition เงื่อนไข
+
+pton_is_itim = True 
+pton_aroi_itim = False
+pton_rakatukk = True
+if pton_is_itim and pton_aroi_itim:
+    print("จะกินพี่ต้น")
+elif:pton_rakatukk:
+    print("กินเพราะราคาถูก")
+else:
+    print("ทิ้ง")
+
